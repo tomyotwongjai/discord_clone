edf2e4a9989a506413963a31c89fab72c3eb36c7
Tom Yotwongjai <munch2018@gmail.com>
2022-04-08 08:08:27 +0200

    predeploy

---
+++ src/components/PersistLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+const PersistLogin = () => {
+  return <div>PersistLogin</div>;
+};
+
+export default PersistLogin;
--- src/constants/images.js
+++ src/constants/images.js
@@ -3,7 +3,6 @@ import hero_img_1 from '../assets/hero_img_1.svg';
 import hero_img_2 from '../assets/hero_img_2.svg';
 import hero_img_3 from '../assets/hero_img_3.svg';
 import channel_img from '../assets/channel_img.png';
-import qr_img from '../assets/qr.png';
 
 export default {
   header_logo,
@@ -11,5 +10,4 @@ export default {
   hero_img_2,
   hero_img_3,
   channel_img,
-  qr_img,
 };
